29ceafdf7a842835deb3112459a0a427950b72dc
wisdom ugo <wisdomjudeson@gmail.com>
2019-11-05 21:29:19 -0800

    completed module 4

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 
+import {Album} from '../album';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
